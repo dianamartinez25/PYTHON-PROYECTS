4dd7a40a28ab098a2be1b1b492f7a179e863c051
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-14 17:53:26 +0100

    Add files via upload

---
+++ actividadrange3.py
@@ -0,0 +1,12 @@
+# programa para gestionar los invitados de la fiesta
+gente = int(input("¿Cuánta gente acudira a la fiesta?: "))
+MAX_INVITADOS = 5
+if gente > MAX_INVITADOS:
+    print(f"Lo siento, solo {MAX_INVITADOS} personas pueden acudir a la fiesta. ")
+else:
+    # preguntar al usuario por el nombre de cada invitado. - repetir X veces.
+    # imprimir un mensaje individual para cada invitado.
+
+
+    # avanzado - guardar cada nombre en una lista e imprimir la lista al final del programa
+    #piesta - invitados.append()
