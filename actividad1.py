7a29a45917dc36e2add890e5d7b04b54884cfccf
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-14 16:49:43 +0100

    Add files via upload

---
+++ actividad1.py
@@ -0,0 +1,4 @@
+# Este programa va acalculcar un salario
+salario = int(input("¿Cuánto cobras mensualmente?" ))
+resultado = salario*10
+print(f"Podrías ganar {resultado}$ si fueras experto en Pyhton.")
---
+++ test2.py
@@ -0,0 +1,13 @@
+a = 10
+print(id(a))
+a = 11
+isinstance(a, int)
+type(a)
+a = 10
+if (a == 10): 
+    print("es igual a 10")
+elif(a == 12):
+    print("12")
+else:
+    print("otros")
+    
