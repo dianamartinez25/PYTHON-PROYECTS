e6c6428605286dc7309c2c5a0dc1a07932ad1d1b
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-15 17:50:08 +0100

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,18 @@
+cosa = [ 5, 1 , 20 , 300 ]
+#print(cosa[1:4])
+#print(cosa[-1])
+
+#cosa.append(50)
+
+#cosa.insert(2,23)
+#print(cosa)
+# cosa.remove("aba")
+# print(cosa)
+# cosa.sort(reverse=True)
+#  print(cosa)
+lista=[5, 7, 3, 12, 5, 6, 11, 1, 15]
+lista.append(23)
+lista.remove(11)
+lista.insert(4,50)
+lista.sort(reverse=True)
+print(lista)
