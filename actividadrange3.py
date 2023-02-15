1f0ec84a7fe6b31fc46e9cfebad37e7d2a422f97
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-15 15:18:17 +0100

    Update actividadrange3.py

--- actividadrange3.py
+++ actividadrange3.py
@@ -1,41 +1,17 @@
-# programa para gestionar los invitados de la fiesta
-gente = int(input("¿Cuánta gente acudira a la fiesta?: "))
-MAX_INVITADOS = 5
-if gente > MAX_INVITADOS:
-    print(f"Lo siento, solo {MAX_INVITADOS} personas pueden acudir a la fiesta. ")
-else:
-    # preguntar al usuario por el nombre de cada invitado. - repetir X veces.
-    # imprimir un mensaje individual para cada invitado.
-
-
-    # avanzado - guardar cada nombre en una lista e imprimir la lista al final del programa
-    #piesta - invitados.append()
-
-    
-    
-    
-    
-    
-    
-    #TRABAJO HECHO EN CASA:
+#TRABAJO HECHO EN CASA:
     
     
 # programa para gestionar los invitados de la fiesta
 print("Estamos organizando los preparativos para la fiesta que daremos este fin de semana. A continuación os haremos unas preguntas: ")
 gente = int(input("¿Cuánta gente acudirá a la fiesta?: "))
-MAX_INVITADOS  = 15
-#respuesta = ["{gente}>{MAX_INVITADOS}"]
-if gente > MAX_INVITADOS:
-    #(TIENE ALGÚN FALLO) == print("Lo siento, solo {MAX_INVITADOS} personas pueden acudir a la fiesta. ")
+MAX_INVITADOS  = 10
 
-#["{gente}>{MAX_INVITADOS}"]:
+
     
-#if gente == respuesta:
+
     
 
 
-#si  gente  >  MAX_INVITADOS :
-    
 #más :
     # preguntar al usuario por el nombre de cada invitado. - repetir X veces.
     # imprimir un mensaje individual para cada invitado.
