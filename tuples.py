25e4b1a8cbae9373f0d6964b554a28c98ff2eb31
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-16 15:55:22 +0100

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# Crear un tuple para controlar las estaciones del año. 
+estaciones= ("invierno", "primavera", "verano", "otoño")
+for e in estaciones:
+    # Mostrar toda la lista de estaciones
+    print(e)
+print()
+# Mostrar “verano”
+print(estaciones[2])
+
+
+
+
