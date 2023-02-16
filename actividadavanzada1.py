0b3c59123a3ad4ab3d11979eb27de0f4f2cb382b
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-16 17:43:23 +0100

    Delete actividadavanzada1.py

--- actividadavanzada1.py
+++
@@ -1,7 +0,0 @@
-print("Control de asistencia a clase.")
-lista = ["Ana", "vicente", "Juan", "Marina", "Fernando", "Sarai"]
-# Ordenar lista alfabeticamente.
-#lista.sort()
-if lista == asistencia:
-    
-asistencia = input("¿Ha venido (lista)a clase? (y/n)")
