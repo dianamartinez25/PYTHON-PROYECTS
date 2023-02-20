1d88e2dd104325201b78bd7e41f183f020c9f312
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-20 17:48:53 +0100

    Bucles

---
+++ bucle.py
@@ -0,0 +1,50 @@
+# FOR
+# for n in numeros:
+#     print(n)
+# datos = ["tenis, 23, 5.345, Jon"]
+# for dato in datos:
+#     print(dato)
+
+#RANGE
+# a = range(10)
+#  print(type(a))
+# print(a)  
+# for i in range (10):
+#     print(i)
+
+#WHILE
+# while True:
+#     print("Hola")
+    #pass
+
+# i = 0
+# while i<10:
+#     print("Hola" + str(i))
+#     i=i+1
+
+#  a=10
+#  while a>0:
+#     print("número" + str(a))
+#     a=a-1
+# #     if a ==5:
+# #         print("a es 5")
+# #         break
+
+# else:
+#     print("else")
+#  print("fin de bucle")
+
+
+
+# x = 12
+# while x>0:
+#     print("númeero" + str(x))
+#     x = x+1
+#se crea un infinito.
+
+c = 100
+while c > 200:
+    print("buena suerte")
+else:
+    c < 10
+    print("ok")
---
+++ importar.py
@@ -0,0 +1,16 @@
+#PASO 1 
+#help("modules")
+
+#####
+# import random as r
+# from random import f 
+
+#PASO 2
+# import random
+# print(dir(random))
+# x = random.random()
+# y = random.randint(10, 100)
+# print(y)
+# frutas = ["manzana", "platano", "kiwi"]
+# z = random.choice(frutas)
+# print(z)
