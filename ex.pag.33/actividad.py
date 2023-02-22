601368c3158c2d89b39622a942dd089e0b52cb5c
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-22 17:29:38 +0100

    Create actividad.py

---
+++ ex.pag.33/actividad.py
@@ -0,0 +1,4 @@
+#Usando replace(), cambiar el texto para que sea correcto. Además, usar el nombre original de “Python” en lugar de Píton.
+texto = "Pitón es un lenguaje de programación. Pitón está usado para la automación, análisis de datos e incluso la creación de páginas webs. Pitón fue creado por Bill Gates en 1960. Pitón es difícil de usar."
+texto = texto.replace("Pitón", "Python")
+print(texto)
