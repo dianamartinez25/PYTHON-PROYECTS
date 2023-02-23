07f8b99172d85538c7d6c766004fc6028764044d
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-02-23 17:52:58 +0100

    Add files via upload

---
+++ ex.pag.33/actividad0.1.py
@@ -0,0 +1,13 @@
+#Gestión de los correos electrónicos
+emails = ["jon.smith@microsoft.com", "maria.fernandez@microsoft.com", "david@microsoft.com", "isabel@microsoft.es","alfonso@gmail.com"]
+
+# Generar un informe con:
+# - los nombres de los usuarios
+# - los dominios únicos (sin repetir el dominio)
+
+nombres = ("maria", "jon", "david")
+# con los nombres, generar un texto en formato CSV con nuevos correos e.g maria@nazaret.eus,jon@nazaret.eus,david@nazaret.eus
+
+email1 = "jon.smith@microsoft.com"
+
+email2 = "maria.fernandez@gmail.com"
