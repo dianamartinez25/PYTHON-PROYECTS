d41b06e1c30fb81edd67bdfb7faef32fd1b3ffcb
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-03-02 14:57:26 +0100

    Update ex.pag.46.py

--- ex.pag.46.py
+++ ex.pag.46.py
@@ -1,12 +1,16 @@
 #Como un arquitecto quiero calcular metros cuadrados (m2) de una Superticie (largo y ancho)
 #para no tener que calcularlo de forma manual y posiblemente calcularlo incorrectamente.
-def superficie(n,m):
-    return n * m 
+
 import time 
 DORMIR = 0.30
-l = float(input("Dame el largo: "))
-time.sleep(DORMIR)
-a = float(input("Dame el ancho: "))
+
+def superficie(n,m):
+    return n * m 
+
+
 if __name__ == "__main__":
+   l = float(input("Dame el largo: "))
+   time.sleep(DORMIR)
+   a = float(input("Dame el ancho: "))
    r = superficie(l, a)
    print(f"Tu superficie es de {r} m2")
